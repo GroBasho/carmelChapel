a0dfa32649cb9590aaa1f680761c23a6a3f5ea6f
aniket15desai <aniketsardesai44@gmail.com>
2024-05-03 17:38:36 +0530

    Navbar completed

--- app/contact/page.js
+++ app/contact/page.js
@@ -13,7 +13,7 @@ const Contact = () => {
           <form className='flex flex-col justify-center items-center text-left mx-6'>
             <label
               htmlFor='first-name'
-              className='text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
+              className='text-sm font-medium leading-6 text-[#80011F] text-left w-full'
             >
               Name
             </label>
@@ -22,11 +22,11 @@ const Contact = () => {
               name='first-name'
               id='first-name'
               autoComplete='given-name'
-              className='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
+              className='w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
             />
             <label
               htmlFor='email'
-              className='text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
+              className='text-sm font-medium leading-6 text-[#80011F] text-left w-full'
             >
               Email
             </label>
@@ -35,11 +35,11 @@ const Contact = () => {
               name='email'
               id='email'
               autoComplete='email'
-              className='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
+              className='w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
             />
             <label
               htmlFor='subject'
-              className='text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
+              className='text-sm font-medium leading-6 text-[#80011F] text-left w-full'
             >
               Subject
             </label>
@@ -48,11 +48,11 @@ const Contact = () => {
               name='subject'
               id='subject'
               autoComplete='subject'
-              className='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
+              className='w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
             />
             <label
               htmlFor='about'
-              className='block text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
+              className='block text-sm font-medium leading-6 text-[#80011F] text-left w-full'
             >
               About
             </label>
@@ -60,9 +60,9 @@ const Contact = () => {
               id='about'
               name='about'
               rows='3'
-              className='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1]'
+              className='w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1]'
             ></textarea>
-            <div className='w-9/12 flex items-center justify-end'>
+            <div className='w-full flex items-center justify-end'>
               <button
                 type='submit'
                 className='my-2 rounded-md bg-[#80011F] px-10 py-2 text-sm font-semibold text-[#FFF5E1] shadow-sm hover:border-2 hover:border-[#80011F] hover:bg-[#FFF5E1] hover:text-[#80011F]'
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,71 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Logo from '@/images/logo.png';
 import Image from 'next/image';
+import Link from 'next/link';
+import { navLink } from '@/constants';
+import { usePathname } from "next/navigation";
+import { Bars3Icon, XCircleIcon } from '@heroicons/react/24/outline'
 
 const Navbar = () => {
+    const pathname = usePathname();
+    const [toggleDropdown, settoggleDropdown] = useState(false);
     return (
-        <div className='flex h-36 w-full bg-white sticky justify-start items-center px-5'>
-            <Image
-                src={Logo}
-                height={100}
-                width={100}
-                alt='Logo'
-                className='ms-14'
-            />
+        <div className='flex sm:h-24 h-20 w-full bg-white sticky justify-between items-center'>
+            <Link href='/' className='cursor-pointer md:w-4/12 px-5'>
+                <Image
+                    src={Logo}
+                    height={80}
+                    width={80}
+                    alt='Logo'
+                    className='md:ms-14 h-12 w-12 sm:h-20 sm:w-20'
+                />
+            </Link>
+            <div className='sm:flex justify-start items-center gap-28 text-black w-8/12 hidden px-5'>
+                {navLink.map((link) => (
+                    <Link href={link.href}
+                        key={link.name}
+                        className={`${pathname == link.href ? 'Livvic-Bold text-[#4169E2]' : 'Livvic-Medium text-[#1E1E1E]'} cursor-pointer text-xl hover:text-[#4169E2]`}
+                    >
+                        {link.name}
+                    </Link>
+                ))}
+            </div>
+
+            <nav className='sm:hidden flex relative pr-5'>
+                <div className='flex w-full'>
+                    {toggleDropdown == false ? (
+                        <Bars3Icon
+                            width={40}
+                            height={40}
+                            onClick={() => settoggleDropdown((prev) => !prev)}
+                        />
+                    ) : (
+                        <XCircleIcon
+                            width={30}
+                            height={30}
+                            onClick={() => settoggleDropdown((prev) => !prev)}
+                        />
+                    )}
+
+                    {toggleDropdown && (
+                        <div className='absolute right-0 top-14 w-screen p-5  bg-white flex flex-col gap-2 justify-end items-end'>
+                            {navLink.map((link) => {
+                                return (
+                                    <Link
+                                        href={link.href}
+                                        key={link.name}
+                                        className={`${pathname == link.href ? 'Livvic-Bold text-[#4169E2]' : 'Livvic-Medium text-[#1E1E1E]'} cursor-pointer text-xl hover:text-[#4169E2]`}
+                                        onClick={() => settoggleDropdown((prev) => !prev)}
+                                    >
+                                        {link.name}
+                                    </Link>
+                                );
+                            })}
+                        </div>
+                    )}
+                </div>
+            </nav>
         </div>
     )
 }
