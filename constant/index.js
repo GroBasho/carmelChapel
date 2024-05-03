11004bb5da6c89d128bc2c1928d5e0b550b067a7
aniket15desai <aniketsardesai44@gmail.com>
2024-05-03 17:38:36 +0530

    Navbar completed

---
+++ constant/index.js
@@ -0,0 +1,6 @@
+export const navLink = [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Gallery", href: "/gallery" },
+    { name: "Contact Us", href: "/contact" },
+];
