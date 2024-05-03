6f7033e198c039dd6d297b62ed35bd4cd90028a2
aniket15desai <aniketsardesai44@gmail.com>
2024-05-03 13:01:45 +0530

    Fixed

--- app/contact/page.js
+++ app/contact/page.js
@@ -12,8 +12,8 @@ const Contact = () => {
         <div className='sm:w-[55%]  w-full h-full text-center self-center p-6 mb-4'>
           <form className='flex flex-col justify-center items-center text-left mx-6'>
             <label
-              for='first-name'
-              class='text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
+              htmlFor='first-name'
+              className='text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
             >
               Name
             </label>
@@ -21,12 +21,12 @@ const Contact = () => {
               type='text'
               name='first-name'
               id='first-name'
-              autocomplete='given-name'
-              class='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
+              autoComplete='given-name'
+              className='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
             />
             <label
-              for='email'
-              class='text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
+              htmlFor='email'
+              className='text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
             >
               Email
             </label>
@@ -34,12 +34,12 @@ const Contact = () => {
               type='email'
               name='email'
               id='email'
-              autocomplete='email'
-              class='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
+              autoComplete='email'
+              className='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
             />
             <label
-              for='subject'
-              class='text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
+              htmlFor='subject'
+              className='text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
             >
               Subject
             </label>
@@ -47,12 +47,12 @@ const Contact = () => {
               type='text'
               name='subject'
               id='subject'
-              autocomplete='subject'
-              class='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
+              autoComplete='subject'
+              className='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1] pl-2'
             />
             <label
-              for='about'
-              class='block text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
+              htmlFor='about'
+              className='block text-sm font-medium leading-6 text-[#80011F] text-left w-9/12'
             >
               About
             </label>
@@ -60,12 +60,12 @@ const Contact = () => {
               id='about'
               name='about'
               rows='3'
-              class='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1]'
+              className='w-9/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#80011F] placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-[#FFF5E1]'
             ></textarea>
             <div className='w-9/12 flex items-center justify-end'>
               <button
                 type='submit'
-                class='my-2 rounded-md bg-[#80011F] px-10 py-2 text-sm font-semibold text-[#FFF5E1] shadow-sm hover:border-2 hover:border-[#80011F] hover:bg-[#FFF5E1] hover:text-[#80011F]'
+                className='my-2 rounded-md bg-[#80011F] px-10 py-2 text-sm font-semibold text-[#FFF5E1] shadow-sm hover:border-2 hover:border-[#80011F] hover:bg-[#FFF5E1] hover:text-[#80011F]'
               >
                 Send
               </button>
